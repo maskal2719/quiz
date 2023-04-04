307274fd2bc41154a9badcf4b4be754b11fe1fb9
Stanislav Moskalenko <maskal2719@gmail.com>
2023-04-04 16:49:25 +0300

    fix layout

--- src/Components/QuestionCard.tsx
+++ src/Components/QuestionCard.tsx
@@ -11,18 +11,23 @@ type QuestionCardPropsType = {
 const QuestionCard: React.FC<QuestionCardPropsType> = ({question, nextQuestion, questions, step}) => {
 
     return (
+
         <div className={'card'}>
-            <div>Вопрос номер {step} из {questions.length}</div>
-            <div className={'questions'}>
-                <h2>{question?.question}</h2>
-                <ul>
-                    {
-                        question?.answers?.map((el, index) =>
-                            <li key={index} className={'question'} onClick={() => nextQuestion(el)}>{el}</li>
-                        )
-                    }
-                </ul>
-            </div>
+            {
+                !questions?.length ? 'Loading...' : <>
+                    <div>Вопрос номер {step} из {questions.length}</div>
+                    <div className={'questions'}>
+                        <h2>{question?.question}</h2>
+                        <ul>
+                            {
+                                question?.answers?.map((el, index) =>
+                                    <li key={index} className={'question'} onClick={() => nextQuestion(el)}>{el}</li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                </>
+            }
         </div>
     );
 };
